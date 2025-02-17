a1eab8492e2d5e770ef5f7db6903966ce969a4e3
BySenko0 <solisske@icloud.com>
2025-02-16 20:22:05 -0600

    metodo delete

--- API de Gestión de Tareas/entornotareas/main.py
+++ API de Gestión de Tareas/entornotareas/main.py
@@ -42,3 +42,12 @@ def actualizar_tarea(id: int, tarea_actualizada: dict):
             tareas[index].update(tarea_actualizada)
             return tareas[index]
     raise HTTPException(status_code=404, detail="Tarea no encontrada")
+
+# Eliminar una tarea
+@app.delete("/tareas/{id}", tags=["Operaciones CRUD"])
+def eliminar_tarea(id: int):
+    for index, tarea in enumerate(tareas):
+        if tarea["id"] == id:
+            del tareas[index]
+            return {"message": "Tarea eliminada"}
+    raise HTTPException(status_code=404, detail="Tarea no encontrada")
